5f3fb65a4f366232dff56e5989ff753149952276
MarcinTheOriginal <mmanicki@gmail.com>
2021-09-04 19:43:28 +0200

    Podwaliny pod filtrowanie

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 const toTheTopButton = document.querySelector('.scrollTop');
+const filterButton = document.querySelector('.arrowFilters');
+const arrowFilter = document.querySelector('.fa-chevron-left')
+const filters = document.querySelector('.filters');
+// dodać zamykanie filtra po kliknięciu poza nim gdy jest otwarty
 
 window.addEventListener('scroll', () => {
   if(window.pageYOffset > 500) {
@@ -13,3 +17,18 @@ const scrollTop = () => {
 }
 
 toTheTopButton.addEventListener('click', scrollTop);
+
+// Filtry
+
+const toggleFilters = () => {
+
+  filters.classList.toggle('activeFilters');
+  arrowFilter.classList.toggle('arrowRotate');
+  if (filterButton.classList.contains("arrowRotate")) {
+
+  } else {
+
+  }
+}
+
+filterButton.addEventListener('click', toggleFilters);
